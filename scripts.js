3d494fd4b731154e3c3986cd47080982ded8fe91
geraldinebil <113667275+geraldinebil@users.noreply.github.com>
2023-05-31 12:00:07 +0200

    listage des champions

--- scripts.js
+++ scripts.js
@@ -8,8 +8,25 @@ l
 const championsContainer = document.getElementById('champions')
 const championDetailsContainer = document.getElementById('champion-details')
 
-
+//Appel de tout les champions
+const championsData = ['http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/champion/Aatrox.json']
 
 
 //Les appels à l'API
-await fetch('http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/champion', 'GET', fetchChampion);
+await fetch('http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/champion/Aatrox.json', 'GET', fetchChampion);
+fetch('http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/champion/Aatrox.json/?limit=200', 'GET',setAllChampion);
+
+//Récupère tous les champions et fait une boucle dessus pour mon slider a droite
+function setAllChampion(){
+    allChampion = [];
+    let champ;
+    let response = JSON.parse(this.response)
+    response.results.map((lol, index) => {
+        fetch(lol.url, 'GET', (e) => {
+            poke = JSON.parse(e.originalTarget.response)
+            allPokemon.push(champ)
+        })
+    })
+}
+
+fetchChampion();
