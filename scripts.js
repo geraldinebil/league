55e46f40276ea60367363dcb2f2b6fbb6c6d7e6d
geraldinebil <113667275+geraldinebil@users.noreply.github.com>
2023-05-31 11:37:35 +0200

    listage des champions avec l'API

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', async () => {
 //Création des variables
-
+l
 
 
 //Récupération des Élement DOM
 })
-
+const championsContainer = document.getElementById('champions')
+const championDetailsContainer = document.getElementById('champion-details')
 
 
 
 
 //Les appels à l'API
+await fetch('http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/champion', 'GET', fetchChampion);
